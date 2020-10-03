83ff67390eb839fc3e13dffb971e6071f051707c
ZhurydAndrei <zhuryd.andrei@gmail.com>
2020-10-03 23:28:14 +0300

    feat: add solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,15 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if(
+    typeof sampleActivity !== 'string' || 
+    sampleActivity <= 0 || 
+    sampleActivity > MODERN_ACTIVITY || 
+    !parseFloat(sampleActivity))
+    {
+      return false
+  }else {
+    let age = Math.log2(MODERN_ACTIVITY/parseFloat(sampleActivity)) / (0.693/HALF_LIFE_PERIOD)
+    return age
+  }
 };
